516a7ce3bdb4a2506256364ec86452e7a2fb4c14
Grant Wade <gwade@vandals.uidaho.edu>
2019-10-07 00:15:46 -0400

    Updated TrivialProcessor to update a store if one exists, and have multiple onRecord callback methods

--- core/src/main/java/com/brokencircuits/kissad/kafka/TrivialProcessor.java
+++ core/src/main/java/com/brokencircuits/kissad/kafka/TrivialProcessor.java
@@ -1,23 +1,57 @@
 package com.brokencircuits.kissad.kafka;
 
-import java.util.function.BiConsumer;
-import lombok.RequiredArgsConstructor;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.streams.processor.Processor;
 import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.logging.log4j.util.BiConsumer;
+import org.apache.logging.log4j.util.TriConsumer;
 
-@RequiredArgsConstructor
-public class TrivialProcessor<K,V> implements Processor<K, V> {
+@Slf4j
+@Builder
+public class TrivialProcessor<K, V> implements Processor<K, V> {
 
-  final private BiConsumer<K,V> onRecord;
+  private TriConsumer<K, V, V> onRecordActionWithPrevious;
+  private BiConsumer<K,V> onRecordAction;
+  private String storeName;
+  @Builder.Default
+  private KeyValueStore<K, V> internalStore;
 
   @Override
-  public void init(ProcessorContext processorContext) {
-
+  public void init(ProcessorContext context) {
+    if (storeName != null) {
+      internalStore = (KeyValueStore) context.getStateStore(storeName);
+    }
   }
 
   @Override
-  public void process(K k, V v) {
-    onRecord.accept(k, v);
+  public void process(K key, V newValue) {
+
+    log.debug("Processing {} | {}", key, newValue);
+    V oldValue = null;
+    if (storeName != null) {
+      log.debug("Updating store with new value: {} | {}", key, newValue);
+      oldValue = internalStore.get(key);
+      internalStore.put(key, newValue);
+    }
+
+    if (onRecordActionWithPrevious != null) {
+      try {
+        onRecordActionWithPrevious.accept(key, oldValue, newValue);
+      } catch (RuntimeException e) {
+        log.error("Exception in onRecordActionWithPrevious: ", e);
+      }
+    }
+
+    if (onRecordAction != null) {
+      try {
+        onRecordAction.accept(key, newValue);
+      } catch (RuntimeException e) {
+        log.error("Exception in onRecordAction: ", e);
+      }
+    }
+    log.debug("Finished processing {} | {}", key, newValue);
   }
 
   @Override
